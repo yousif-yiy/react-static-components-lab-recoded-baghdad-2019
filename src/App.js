8dae3840a26388b7a1a956e9f220ad0b8679159e
yousif diyhaa <yousif7733@gmail.com>
2020-01-21 19:28:43 +0000

    Automatically backed up by Learn

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import CatComponent from './CatComponent.js'
-import FordQuoteComponent from './FordQuoteComponent.js'
 import MouseComponent from './MouseComponent.js'
 
 class App extends Component {
